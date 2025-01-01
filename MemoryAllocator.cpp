f484febed470818a4e1f4a68496a2716465a07d6
Szlanta Arkadiusz <87637585+aroesz98@users.noreply.github.com>
2025-01-01 21:35:25 +0100

    Update MemoryAllocator.cpp

--- MemoryAllocator.cpp
+++ MemoryAllocator.cpp
@@ -4,9 +4,6 @@
  * Date: 05 Dec 2024
  *
  * License:
- * This source code is provided for hobbyist and private use only.
- * Any commercial or industrial use, including distribution, reproduction, or
- * incorporation in commercial or industrial products or services is prohibited.
  * Use at your own risk. The author(s) hold no responsibility for any damages
  * or losses resulting from the use of this software.
  *
